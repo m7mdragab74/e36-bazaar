aae34e9246b7af1e3cff433b4627065ea5f20895
m7mdragab <mr341349@gmail.com>
2024-09-05 12:48:27 +0300

    done app ui

--- lib/views/details_page.dart
+++ lib/views/details_page.dart
@@ -1,12 +1,76 @@
+import 'package:e36_bazzar/widget/signup/custom_button.dart';
 import 'package:flutter/material.dart';
 
 class DetailsPage extends StatelessWidget {
   const DetailsPage({super.key});
 
+  static const TextStyle titleStyle = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 27,
+  );
+
+  static const TextStyle descriptionStyle = TextStyle(
+    fontSize: 16,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text(
+          'M3-E36',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(16.0),
+        children: [
+          Image.asset(
+            'assets/e36.jpg',
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(height: 16),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'BMW',
+                style: titleStyle,
+              ),
+              Text(
+                '\$27,199',
+                style: titleStyle,
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          const Text(
+            'BMW M3 Coupe - Manual - E36',
+            style: titleStyle,
+          ),
+          const SizedBox(height: 10),
+          const Text(
+            'When it was first introduced in 1992, the E36 M3 was initially only available as a coupe. The E36 M3 Manual coupe was initially offered with a 5-speed manual gearbox, but from the 1996 model year, a 6-speed manual became standard for European models (North American examples retained the 5-speed). The 3.0L inline six used between 1992 and 1995 produced 286hp, while the later 3.2L inline six produced 316hp.',
+            style: descriptionStyle,
+          ),
+          const SizedBox(height: 10),
+          const Text(
+            'Quantity: 0',
+            style: titleStyle,
+          ),
+          const SizedBox(height: 10),
+          CustomButton(
+            textColor: Colors.white,
+            label: 'Done',
+            height: 22,
+            fontSize: 18,
+            onTap: () {},
+          ),
+        ],
+      ),
     );
   }
 }
